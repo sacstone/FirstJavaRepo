153f491bb9984ff9ff9815508651654a740b11c2
user <user@5CD401LP8>
2016-01-20 21:22:15 -0600

    Test

--- sample/src/sample/Array.java
+++ sample/src/sample/Array.java
@@ -9,6 +9,7 @@ public class Array {
 		for (int i = 0; i < ramesh.length; i ++) {
 			
 			System.out.println("value is: " + ramesh[i]+ "\n");
+			System.out.println("Testing Repository");
 			
 		}
 
