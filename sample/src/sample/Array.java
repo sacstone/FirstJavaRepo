51d5129fb6fc0d4b4e8bbd148279e11b3c6dda34
user <user@5CD401LP8>
2016-01-20 21:19:32 -0600

    First commit to repository

---
+++ sample/src/sample/Array.java
@@ -0,0 +1,17 @@
+package sample;
+
+public class Array {
+
+	public static void main(String[] args) {
+	
+		int[] ramesh = {1, 2, 3, 4, 5};
+		
+		for (int i = 0; i < ramesh.length; i ++) {
+			
+			System.out.println("value is: " + ramesh[i]+ "\n");
+			
+		}
+
+	}
+
+}
