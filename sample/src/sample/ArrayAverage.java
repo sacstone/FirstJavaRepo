096f7701a00a12920a7ed4c5c81e1b8dbcfb8581
Ramesh_Dhanekula <Ramesh_Dhanekula@WN7X64-9TW3562.aus.amer.dell.com>
2016-01-22 08:54:08 -0600

    Test

---
+++ sample/src/sample/ArrayAverage.java
@@ -0,0 +1,35 @@
+package sample;
+
+import java.io.*;
+
+public class ArrayAverage {
+
+	public static void main(String[] args) throws IOException {
+		
+		InputStreamReader inStream = new InputStreamReader(System.in);
+		BufferedReader stdin = new BufferedReader(inStream);
+		
+		String inData;
+		int k=1;
+		
+		int[][] n = new int[4][5];
+		for(int i = 0; i < 4; i ++) {
+			
+			for (int j = 0; j < 5; j++) {
+				
+				
+				System.out.println("Enter number: " );
+				
+				inData = stdin.readLine();
+				int n1 = Integer.parseInt(inData);
+				
+			}
+			
+			
+			
+		}
+				
+
+	}
+
+}
